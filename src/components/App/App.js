9df411630a0f31f83ba29b56d5e3371c2a7b97ee
Illia Voloshchuk <illia.voloshchuk@eleks.com>
2019-05-02 15:14:30 +0300

    build: setup build, add eslint, absolute paths, scss support

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import CoreLayout from 'layouts/CoreLayout';
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <CoreLayout />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import 'styles/core.scss';
+import styles from './CoreLayout.scss';
+
+export const CoreLayout = () => (
+  <div>
+    <p className={styles.peshka}>Hello</p>
+  </div>
+);
+
+export default CoreLayout;
